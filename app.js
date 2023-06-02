dae4ebe6efc00d006cda44bd37c97c01ef668825
eric kisendi <erickisendi18@gmail.com>
2023-06-02 15:12:22 +0300

    update

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-const{readFileSync, writeFileSync} = require('fs')
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url == '/'){
+        res.end('Welcome again to the world')
+    }
+    if(res.url == '/about'){
+        res.end('Here is our homepage')
+    }
+    res.end(`
+        <h1>Oops!</h1>
+        <p>We cant find the page you are looking for</p>
+        <a href="/">Back home</a>
+    `)
+})
+
+server.listen(5000);

ef93e61ec98dc411cd750212eb3b26943ad80b72
eric kisendi <erickisendi18@gmail.com>
2023-06-12 08:45:39 +0300

    streams

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-const {writeFileSync} = require('fs')
+const http = require('http');
+const fs = require('fs');
 
-for (let i = 0; i < 10000; i++) {
-    writeFileSync('./content/big.txt', `Hello there world ${i}\n`, {flag: 'a'})
-}
+const server = http.createServer((req, res) =>{
+    //const text = fs.readFileSync('./content/big1.txt', 'utf8')
+    //console.log('I\'m requested')
+    //res.end(text)
+
+//The following code displays chunked in transfer-encoding in dev tools
+
+    const fileStream = fs.createReadStream('./content/big1.txt', 'utf8')
+    fileStream.on('open',() =>{
+        fileStream.pipe(res)
+    })
+    fileStream.on('error',(err) => {
+       res.end(err)
+    })
+})
+
+server.listen(4000)
